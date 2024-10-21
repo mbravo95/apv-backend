61b7e90bf526bf43a76510918ccf550895bb6c9e
mbravo95 <88863037+mbravo95@users.noreply.github.com>
2024-10-21 10:19:29 -0300

    Forzando cors

--- index.js
+++ index.js
@@ -17,12 +17,7 @@ const dominiosPermitidos = [process.env.FRONTEND_URL, 'http://172.30.3.31'];
 
 const corsOptions = {
     origin: function(origin, callback){
-        console.log(origin);
-        if(dominiosPermitidos.indexOf(origin) !== -1){
-            callback(null, true);
-        } else {
-            callback(new Error('No permitido por CORS'));
-        }
+        callback(null, true);
     }
 }
 
