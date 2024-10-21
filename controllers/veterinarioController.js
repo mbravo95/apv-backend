92503d8b304c6863f4e29a5f7c6ba26b10ad4a7b
mbravo95 <88863037+mbravo95@users.noreply.github.com>
2024-10-21 10:29:42 -0300

    Me traigo los datos del request

--- controllers/veterinarioController.js
+++ controllers/veterinarioController.js
@@ -5,6 +5,8 @@ import emailRegistro from '../helpers/emailRegistro.js';
 import emailOlvidePassword from '../helpers/emailOlvidePassword.js';
 
 const registrar = async (req,res) => {
+    console.log(req.body);
+    console.log(req.params);
     const { email, nombre } = req.body;
 
     const existeUsuario = await Veterinario.findOne({email});
